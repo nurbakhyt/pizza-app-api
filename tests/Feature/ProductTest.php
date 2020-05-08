001f86507b1bc18c0d5bf21775981eee2230513b
Nurbakhyt <nurbakhyt@gmail.com>
2020-05-09 02:08:12 +0600

    Product model

---
+++ tests/Feature/ProductTest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Product;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class ProductTest extends TestCase
+{
+    use RefreshDatabase;
+
+    protected $product;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->product = Product::create([
+            'name' => 'Pizza 1',
+            'price' => 9.99,
+        ]);
+    }
+
+    /** @test */
+    function a_product_has_name()
+    {
+        $this->assertEquals('Pizza 1', $this->product->name);
+    }
+
+    /** @test */
+    function a_product_has_a_price()
+    {
+        $this->assertEquals(9.99, $this->product->price);
+    }
+}
